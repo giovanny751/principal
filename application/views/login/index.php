b9eca225558f0743041c5b24627a9b64af770667
giovanny751 <giovanny751@hotmail.com>
2015-03-11 09:39:58 -0500

    pricipal
    principal

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -47,19 +47,19 @@
     @media screen and (min-width: 768px) {
 
 
-        .fondo{ 
-            background: url('<?php echo base_url('img/y.png') ?>');
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            background-size: 100% 100%; 
-            top: 0%; 
-            left: 0%; 
-            width: 100%; 
-            height: 90%; 
-            bottom: 0; 
-            /*opacity: 0.8;*/
-            /*               position: fixed; */
-        }
+        /*        .fondo{ 
+                    background: url('<?php echo base_url('img/y.png') ?>');
+                    background-repeat: no-repeat;
+                    background-attachment: fixed;
+                    background-size: 100% 100%; 
+                    top: 0%; 
+                    left: 0%; 
+                    width: 100%; 
+                    height: 90%; 
+                    bottom: 0; 
+                    opacity: 0.8;
+                                   position: fixed; 
+                }*/
         div img.load{
             position: absolute;
             left: 40%;
@@ -105,44 +105,44 @@
 
 </style>
 
-<div class="row fondo" id="cero" style="width: 100%;height: 700px;background-color: #ABE9F5;"> 
+<div class="row" id="cero" style="width: 100%;height: 650px;background-color: red;"> 
     <!--<div id="agrandado" style="width: 16%;background-color: red;position: relative;left: 43%;top: 30%">-->
-        <!--<div class="fondo">-->
-            <!--<div class="cuadro11"></div>
-            <div class="cuadro22"></div>-->
-            <div class="cuadro1"></div>
-            <div class="cuadro1opa"></div>
-            <div class="cuadro2"></div>
-            <div class="cuadro2opa"></div>
-            <div class="cuadro3"></div>
-            <div class="cuadro3opa"></div>
-            <div class="cuadro4"></div>
-            <div class="cuadro4opa"></div>
-            <div class="cuadro5"></div>
-            <div class="cuadro5opa"></div>
-            <div class="cuadro6"></div>    
-            <div class="cuadro6opa"></div>    
-            <div class="cuadro7"></div>    
-            <div class="cuadro7opa"></div>    
-            <div class="cuadro8"></div>    
-            <div class="cuadro8opa"></div>    
-            <div class="cuadro9"></div>    
-            <div class="cuadro9opa"></div>    
-            <div class="cuadro10"></div>    
-            <div class="cuadro10opa"></div>    
-            <div>
-                <img src="<?php echo base_url('img/cubo.png') ?>" alt="Y"  class="cubo">
-            </div>
-            <div>
-                <img src="<?php echo base_url('img/soft.png') ?>" alt="soft"  class="soft">
-            </div> 
-            <div>
-                <img src="<?php echo base_url('img/n.png') ?>" alt="N"  class="N">
-                <img src="<?php echo base_url('img/g.png') ?>" alt="G"  class="G">
-            </div>
-            <input type='hidden' id='repetir'>
+    <div class="fondo">
+        <!--<div class="cuadro11"></div>
+        <div class="cuadro22"></div>-->
+        <div class="cuadro1"></div>
+        <div class="cuadro1opa"></div>
+        <div class="cuadro2"></div>
+        <div class="cuadro2opa"></div>
+        <div class="cuadro3"></div>
+        <div class="cuadro3opa"></div>
+        <div class="cuadro4"></div>
+        <div class="cuadro4opa"></div>
+        <div class="cuadro5"></div>
+        <div class="cuadro5opa"></div>
+        <div class="cuadro6"></div>    
+        <div class="cuadro6opa"></div>    
+        <div class="cuadro7"></div>    
+        <div class="cuadro7opa"></div>    
+        <div class="cuadro8"></div>    
+        <div class="cuadro8opa"></div>    
+        <div class="cuadro9"></div>    
+        <div class="cuadro9opa"></div>    
+        <div class="cuadro10"></div>    
+        <div class="cuadro10opa"></div>    
+        <div>
+            <img src="<?php echo base_url('img/cubo.png') ?>" alt="Y"  class="cubo">
+        </div>
+        <div>
+            <img src="<?php echo base_url('img/soft.png') ?>" alt="soft"  class="soft">
+        </div> 
+        <div>
+            <img src="<?php echo base_url('img/n.png') ?>" alt="N"  class="N">
+            <img src="<?php echo base_url('img/g.png') ?>" alt="G"  class="G">
+        </div>
+        <input type='hidden' id='repetir'>
         <!--</div>-->
-    <!--</div>-->
+    </div>
 </div>
 <div id="uno" class="row" style="width: 100%;height: 500px;background-color: #FABAB3;">
     <div id="agrandado" style="width: 16%;background-color: red;position: relative;left: 67%;top: 30%">
@@ -170,11 +170,60 @@
     </div>
 </div>
 <script src="<?php echo base_url('js/animation.js') ?>"></script>
+<input type="hidden" id="pausa" value="0">
 <script>
     $(".preload, .load").hide();
 
     $(window).scroll(function() {
         var scroll = $(window).scrollTop();
+
+        if (scroll > 286 && $('#pausa').val() == 0) {
+
+            $('#pausa').val('1');
+            $(".fondo").animate({
+                width: "toggle",
+                height: "toggle"
+            }, {
+                duration: 2000,
+                specialEasing: {
+                    width: "linear",
+                    height: "easeOutBounce"
+                },
+                complete: function() {
+//                $( this ).after( "<div>Animation complete.</div>" );
+                }
+            });
+        }
+        if (scroll <= 285 && $('#pausa').val() == 1) {
+
+            $('#pausa').val('0')
+//            $('.fondo').animate({height: '3%',width: '3%'}, "slow");
+
+            $(".fondo").animate({
+                width: ["toggle", "swing"],
+                height: ["toggle", "swing"],
+                duration: 2000,
+                opacity: "toggle"
+            },{
+                duration: 2000,
+                specialEasing: {
+                    width: "linear",
+                    height: "easeOutBounce"
+                },
+                complete: function() {
+//                $( this ).after( "<div>Animation complete.</div>" );
+                }
+            });
+
+
+        }
+//        if(scroll <= 341){
+//            $('.fondo').animate({height: '25%',width: '25%'}, "slow");
+////            $('.fondo').css('width','25%')
+////            $('.fondo').css('height','25%')
+//            
+//        }
+
         if (scroll >= 342 && scroll <= 645) {
             $('#uno').css('background-color', 'black');
         }
